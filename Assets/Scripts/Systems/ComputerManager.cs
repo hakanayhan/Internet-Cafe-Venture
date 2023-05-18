ea5902a04ba54edbffe3047b726dac96ceb14c61
Hakan Ayhan <hakanayhnn@gmail.com>
2023-05-19 01:05:42 +0300

    + LvUp function and ComputerUpgrades class

--- Assets/Scripts/Systems/ComputerManager.cs
+++ Assets/Scripts/Systems/ComputerManager.cs
@@ -17,6 +17,14 @@ public class ComputerManager : MonoBehaviour
         }
         Instance = this;
     }
+    public void LvUp(Computers computer)
+    {
+        computer.level++;
+        computer.computerUpgrades.costMultiplier += 0.1;
+        computer.computerUpgrades.upgradeCostMultiplier += 0.15;
+        computer.cost = computer.baseCost * computer.computerUpgrades.costMultiplier;
+        computer.upgradeCost = computer.baseUpgradeCost * computer.computerUpgrades.upgradeCostMultiplier;
+    }
 }
 
 [Serializable] public class Computers
@@ -24,8 +32,19 @@ public class ComputerManager : MonoBehaviour
     public Computer computerObject;
     public bool isIdle = true;
     public float level;
+    public double baseCost;
     public double cost;
+    public double baseUpgradeCost;
+    public double upgradeCost;
     public int usageTime;
     public double totalCost;
     public CustomerStateMachine customer;
+    public ComputerUpgrades computerUpgrades;
+}
+
+[Serializable] public class ComputerUpgrades
+{
+    public float unlockCost;
+    public double costMultiplier = 1;
+    public double upgradeCostMultiplier = 1;
 }
