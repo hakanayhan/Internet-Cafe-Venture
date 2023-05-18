d354be75e9d9d6758707e85b42dec949aed4d897
Hakan Ayhan <hakanayhnn@gmail.com>
2023-05-19 01:07:48 +0300

    + Refresh and LvUpButton functions
    This commit adds the Refresh function, which updates the computer's information displayed in the Computer Upgrade Window, including the device name, level, cost per second, and upgrade cost.
    Additionally, the LvUpButton function is implemented to check If the player has enough money, it calls the LvUp function in the Computer Manager to perform the upgrade. Afterward, it calls the Refresh function to update the displayed information in the window.

--- Assets/Scripts/UI/ComputerUpgradeWindow.cs
+++ Assets/Scripts/UI/ComputerUpgradeWindow.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 public class ComputerUpgradeWindow : Window
 {
     [SerializeField] GameObject panel;
+    [SerializeField] Text deviceNameText;
+    [SerializeField] Text levelText;
+    [SerializeField] Text costPerSecText;
+    [SerializeField] Text upgradeCostText;
     Computers computer;
+    float level;
+    Currency cost;
+    Currency upgradeCost;
     public static ComputerUpgradeWindow Instance;
     void Awake()
     {
@@ -26,7 +33,24 @@ public class ComputerUpgradeWindow : Window
 
     public void Refresh()
     {
+        int index = ComputerManager.Instance.computers.IndexOf(computer) + 1;
+        string name = "Computer " + index;
+        deviceNameText.text = name;
+        level = computer.level;
+        levelText.text = "Level " + level.ToString();
+        cost = new Currency(computer.cost);
+        costPerSecText.text = cost.ToString() + "/sec";
+        upgradeCost = new Currency(computer.upgradeCost);
+        upgradeCostText.text = upgradeCost.ToString();
+    }
 
+    public void LvUpButton()
+    {
+        if (Wallet.Instance.TryRemoveMoney(upgradeCost))
+        {
+            ComputerManager.Instance.LvUp(computer);
+            Refresh();
+        }
     }
 
     public void OpenWindow(Computers computer)
