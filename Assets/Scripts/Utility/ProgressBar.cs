69df649c62e94008969d04121ba6d012381da6fb
Hakan Ayhan <hakanayhnn@gmail.com>
2023-05-19 02:45:25 +0300

    + ProgressBar script

---
+++ Assets/Scripts/Utility/ProgressBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] Image progressBar;
+
+    public void SetFillAmount(float fillAmount)
+    {
+        progressBar.fillAmount = fillAmount;
+    }
+
+    public void ResetFillAmount()
+    {
+        SetFillAmount(0f);
+    }
+}
